688d2e2157dd390f9e05bb10b5d0eca622dd050c
Michael Fransen <mfransen@strava.com>
2018-02-23 15:28:21 -0800

    Initial Project

---
+++ RxSwiftExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxSwiftExample
+//
+//  Created by Michael Fransen on 2/23/18.
+//  Copyright © 2018 Michael Fransen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
